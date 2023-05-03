34125dceff78c1092832a8334bf8025d61bb373c
Rithvik31 <rithvikchinnu@gmail.com>
2023-05-03 09:21:58 +0530

    update

--- capstone-project/src/main/java/com/ani/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ani.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ani.domain.User;
- 
-public interface UserRepository extends JpaRepository<User, Long> {
- 
-}
